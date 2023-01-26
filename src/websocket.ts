02d01cfa933d5bf54de82979607779d68d049df3
はらたく <70054655+HRTK92@users.noreply.github.com>
2023-01-26 07:08:47 +0000

    不要なimportを削除

--- src/websocket.ts
+++ src/websocket.ts
@@ -1,7 +1,5 @@
 import { WebSocket, WebSocketServer } from 'ws'
-import Events from './events'
 import { v4 as uuidv4 } from 'uuid'
-import { uuid } from 'uuidv4'
 
 class mswc {
   private host: string
